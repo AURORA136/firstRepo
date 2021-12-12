3d9a9b8c23ebe27dffad087bf3d55a2011e9eb41
Zeren Ipek <izeynepzeren@icloud.com>
2021-12-12 04:04:35 +0300

    bank account oluşturdum

--- index.js
+++ index.js
@@ -3,11 +3,28 @@ let BankAcc = function (name, balance) {
     this.balance = balance;
 
     this.deposit = function (depositAmount) {
-        newBalance = this.balance - depositAmount;
+        newBalance = this.balance + depositAmount;
+        console.log("your current balance is" + newBalance)
+
+        this.balance = newBalance ;
+        console.log(BankAcc);
+    };
+
+    this.withdraw = function (withdrawAmount) {
+        newBalance = this.balance - withdrawAmount;
         console.log("your current balance is" + newBalance);
-        if (newBalance <= 0) {
-            console.log("")
+        if (newBalance < 0) {
+            console.log("insufficient");
+        } else {
+            console.log("sufficient");
         }
 
-    }
-}
+        this.balance = newBalance ;
+        console.log(BankAcc;
+    };
+
+};
+
+
+
+
